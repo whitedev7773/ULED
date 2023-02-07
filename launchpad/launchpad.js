b1d83ff80aa6d3b91f1b1a5c42edf2ddb939d948
WhiteDev7773 <jkw04257773@gmail.com>
2023-02-08 00:25:30 +0900

    런치패드 OUTPUT 지원 TEST

--- launchpad/launchpad.js
+++ launchpad/launchpad.js
@@ -1,17 +1,37 @@
 import Color from "./color-code.js";
+import WebMIDI from "./webmidi.js";
 
 // For Module Export
 export default class Launchpad {
 
+    pallete = new Color();
+    web_midi = new WebMIDI();
+    button = {};
     chain = {};
+    logo = null;
+
+    __button_midi_note_data = {
+        // 9 : [chain1,]
+    }
 
     constructor (){
-        console.log("Virtual Laucnhpad Attached");
+        this.logo = document.querySelector("#logo");
         for (var i = 1; i <= 32; i++) {
             this.chain[i] = document.querySelector(`#chain-${i}`);
         }
+        for (var y = 1; y <= 8; y++) {
+            this.button[y] = document.querySelector(`#line-${y}`).children;
+        }
+
+        setTimeout(() => {
+            this.web_midi.Execute(document.querySelector("#midi-out-select"));
+        }, 500);
+
+        console.log("Virtual Laucnhpad Attached");
+        // console.log(this.chain);
     }
 
+    // 가상 런치패드 디자인 변경
     ChangeTypeTo(class_style) {
         document.querySelector("#virtual-launchpad").classList = [class_style];
     }
@@ -21,7 +41,27 @@ export default class Launchpad {
     }
 
     ColorTo(y, x, color_code="0") {
-        document.querySelector(`#line-${y}`).children[Number(x)].style.backgroundColor = new Color().Code(color_code);
+        // console.log(y, x, color_code);
+        switch (y) {
+            case "mc":
+                const chain = Number(x);
+                this.ColorChainTo(x, color_code);
+                break;
+            case "l":
+                this.ColorLogo(color_code);
+                break;
+            default:
+                this.web_midi.SendEvent(Number(`${9-y}${x}`), color_code);
+                this.button[y][x].style.backgroundColor = this.pallete.GetColorByCode(color_code);
+        }
+    }
+
+    ColorChainTo(chain_number, color_code="0") {
+        this.chain[chain_number].style.backgroundColor = this.pallete.GetColorByCode(color_code);
+    }
+    
+    ColorLogo(color_code="0") {
+        this.logo.style.backgroundColor = this.pallete.GetColorByCode(color_code);
     }
 
     Clear(color_code="0") {
@@ -32,32 +72,62 @@ export default class Launchpad {
         }
     }
 
+    // 전체 LED 코드 플레이
     async Play() {
-        var value = document.querySelector("#whole-led").value.split("\n");
+        const lines = document.querySelector("#whole-led").value.split("\n");
+        const commands = {
+            d: "delay",
+            delay: "delay",
+            o: "on",
+            on: "on",
+            f: "off",
+            off: "off"
+        };
 
-        for (var i = 0; i < value.length; i++) {
-            if (value[i] == "") {
+        var all_delay = 0;
+
+        for (const line of lines) {
+            if (!line) {
                 continue;
             }
 
-            var line_split = value[i].split(" ");
-            
-            if (value[i].startsWith("d")) {
-                var delay = line_split[1];
-                await this.Delay(delay);
-                continue;
+            const [command, ...args] = line.split(" ").map(x => x.toLowerCase());
+            const cmd = commands[command];
+            let y, x, colorCode, autoKeywordPosition = 3;
+
+            switch (cmd) {
+                case "delay":
+                    // await this.Delay(Number(args[0]));
+                    all_delay += Number(line.split(" ")[1]);
+                    break;
+                case "on":
+                case "off":
+                    switch (args[0]) {
+                        case "mc":
+                            y = "mc";
+                            x = Number(args[1]);
+                            break;
+                        case "l":
+                            y = "l";
+                            autoKeywordPosition = 2;
+                            break;
+                        default:
+                            y = Number(args[0]);
+                            x = Number(args[1]);
+                    }
+                    switch (args[autoKeywordPosition]) {
+                        case "a":
+                        case "auto":
+                            colorCode = args[autoKeywordPosition + 1];
+                            break;
+                        default:
+                            colorCode = args[autoKeywordPosition];
+                    }
+                    setTimeout(() => {
+                        this.ColorTo(y, x, cmd === "on" ? colorCode : "0");
+                    }, all_delay);
+                    break;
             }
-            else if (line_split[1] == "mc") {
-                var chain = line_split[2];
-                var y = 
-            }
-            else {
-                var y = line_split[1];
-                var x = line_split[2];
-            }
-            var color_code = line_split[4];
-            this.ColorTo(y, x, color_code);
         }
-    }
-
+    }  
 }
